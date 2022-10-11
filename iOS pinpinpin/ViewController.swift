3e514a865c12e40e30eb3302ad174763cbc7a019
jdaipopo <jdaipopo11@gmail.com>
2022-10-11 08:58:42 +0700

    Initial Commit

---
+++ iOS pinpinpin/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS pinpinpin
+//
+//  Created by สรวิชญ์ นิตรัตน์ on 10/10/2565 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ iOS pinpinpinUITests/iOS_pinpinpinUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  iOS_pinpinpinUITestsLaunchTests.swift
+//  iOS pinpinpinUITests
+//
+//  Created by สรวิชญ์ นิตรัตน์ on 10/10/2565 BE.
+//
+
+import XCTest
+
+final class iOS_pinpinpinUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
